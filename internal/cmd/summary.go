4e1878b4aab1f8360608ab51c7d6cb2e282a387b
Philip Meier <github.pmeier@posteo.de>
2025-03-26 23:31:46 +0100

    replace live with summary command

---
+++ internal/cmd/summary.go
@@ -0,0 +1,34 @@
+package cmd
+
+import (
+	"github.com/pmeier/redgiant/internal/summary"
+	"github.com/spf13/cobra"
+)
+
+var summaryViper = NewViper()
+
+var summaryCmd = &cobra.Command{
+	Use:   "summary",
+	Short: "Get summary data",
+	Run: func(cmd *cobra.Command, args []string) {
+		p := summary.SummaryParams{}
+		err := summaryViper.Unmarshal(&p)
+		if err != nil {
+			panic(err.Error())
+		}
+
+		if err := summary.Start(p); err != nil {
+			panic(err.Error())
+		}
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(summaryCmd)
+	defer summaryViper.BindPFlags(summaryCmd.Flags())
+
+	summaryCmd.Flags().String("sungrow-host", "", "Hostname of the Sungrow inverter (required)")
+	summaryCmd.Flags().String("sungrow-password", "pw1111", "Password for the user account of the Sungrow inverter")
+	summaryCmd.Flags().Bool("quiet", true, "Don't output any logs")
+	summaryCmd.Flags().Bool("json", false, "Output summary as JSON. Implies --quiet=true")
+}
