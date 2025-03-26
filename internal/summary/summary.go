ff0000be116faff5008d0645dad6ee3c3dd03434
Philip Meier <github.pmeier@posteo.de>
2025-03-26 23:36:29 +0100

    replace live with summary command (#10)

---
+++ internal/summary/summary.go
@@ -0,0 +1,59 @@
+package summary
+
+import (
+	"encoding/json"
+	"fmt"
+	"os"
+	"text/tabwriter"
+
+	"github.com/pmeier/redgiant/internal/redgiant"
+	"github.com/pmeier/redgiant/internal/utils"
+	"github.com/rs/zerolog"
+)
+
+type SummaryParams struct {
+	SungrowHost     string
+	SungrowPassword string
+	Quiet           bool
+	JSON            bool
+}
+
+func Start(p SummaryParams) error {
+	if p.JSON || p.Quiet {
+		zerolog.SetGlobalLevel(zerolog.Disabled)
+	}
+
+	rg := redgiant.NewRedGiant(p.SungrowHost, p.SungrowPassword)
+	if err := rg.Connect(); err != nil {
+		return err
+	}
+	defer rg.Close()
+
+	d, err := utils.SummaryDevice(rg)
+	if err != nil {
+		return err
+	}
+
+	s, err := rg.Summary(d)
+	if err != nil {
+		return err
+	}
+
+	if p.JSON {
+		b, err := json.Marshal(s)
+		if err != nil {
+			return err
+		}
+		fmt.Println(string(b))
+	} else {
+		w := tabwriter.NewWriter(os.Stdout, 0, 0, 2, ' ', 0)
+		fmt.Fprintf(w, "Grid Power\t%+4.1f kW\n", s.GridPower*1e-3)
+		fmt.Fprintf(w, "Battery Power\t%+4.1f kW\n", s.BatteryPower*1e-3)
+		fmt.Fprintf(w, "PV Power\t%+4.1f kW\n", s.PVPower*1e-3)
+		fmt.Fprintf(w, "Load Power\t%+4.1f kW\n", s.LoadPower*1e-3)
+		fmt.Fprintf(w, "Battery Level\t%4.1f %%\n", s.BatteryLevel*1e2)
+		w.Flush()
+	}
+
+	return nil
+}
