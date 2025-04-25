1f37001e70fd6cf46381813faa7a728d39922990
Philip Meier <github.pmeier@posteo.de>
2025-04-25 17:48:20 +0200

    cleanup

--- http/http.go
+++ http/http.go
@@ -1,61 +1,82 @@
 package http
 
 import (
-	"redgiant"
-	"url"
+	"encoding/json"
+	"errors"
+	"fmt"
+	"io"
+	"net/http"
+	"net/url"
+
+	"github.com/pmeier/redgiant"
 )
 
-type HTTPClient struct {
-	c *http.Client
+type Client struct {
+	c    *http.Client
+	host string
 }
 
-func NewHTTPClient() *HTTPClient {
-	return &HTTPClient{}
+func NewClient(host string) *Client {
+	return &Client{c: &http.Client{}, host: host}
 }
 
-func (hc *HTTPClient) Health() error {
-	return nil
+func assertResponseSuccessful(r *http.Response) error {
+	if r.StatusCode >= 200 && r.StatusCode < 300 {
+		return nil
+	}
+
+	var msg string
+	if c, _ := io.ReadAll(r.Body); len(c) == 0 {
+		msg = r.Status
+	} else {
+		msg = string(c)
+	}
+	return errors.New(msg)
 }
 
-
-func (hc *HTTPClient) getAPI(endpoint string, query url.Values, v any) error {
-	u := url.URL{Scheme: "http", Host: "FIXME", Path: fmt.Sprintf("/api%s", ednpoint)}
-	u.RawQuery = query.Encode()
-
-	r, err := hc.c.Get(u)
+func (hc *Client) Health() error {
+	r, err := hc.c.Get((&url.URL{Scheme: "http", Host: hc.host, Path: "/health"}).String())
 	if err != nil {
 		return err
 	}
 	defer r.Body.Close()
 
-	if !(r.StatusCode >= 200 && r.StatusCode < 300) {
-		if c, _ := io.ReadAll(); len(c) == 0 {
-			msg = r.Status
-		} else {
-			msg = string(c)
-		}
-		return errors.New(msg)
+	return assertResponseSuccessful(r)
+}
+
+func (hc *Client) getAPI(endpoint string, query url.Values, v any) error {
+	u := url.URL{Scheme: "http", Host: hc.host, Path: fmt.Sprintf("/api%s", endpoint)}
+	u.RawQuery = query.Encode()
+
+	r, err := hc.c.Get(u.String())
+	if err != nil {
+		return err
+	}
+	defer r.Body.Close()
+
+	if err := assertResponseSuccessful(r); err != nil {
+		return err
 	}
 
 	return json.NewDecoder(r.Body).Decode(v)
 }
 
-func (hc *HTTPClient) About() (redgiant.About, error) {
+func (hc *Client) About() (redgiant.About, error) {
 	var a redgiant.About
 	return a, hc.getAPI("/about", nil, &a)
 }
 
-func (hc *HTTPClient) State() (redgiant.State, error) {
+func (hc *Client) State() (redgiant.State, error) {
 	var s redgiant.State
 	return s, hc.getAPI("/state", nil, &s)
 }
 
-func (hc *HTTPClient) Devices() ([]redgiant.Device, error) {
+func (hc *Client) Devices() ([]redgiant.Device, error) {
 	var ds []redgiant.Device
 	return ds, hc.getAPI("/devices", nil, &ds)
 }
 
-func dataEndpointQuery(dataType string, deviceID int, lang Language, services ...string) (string, url.Values) {
+func dataEndpointQuery(dataType string, deviceID int, lang redgiant.Language, services []string) (string, url.Values) {
 	e := fmt.Sprintf("/data/%d/%s", deviceID, dataType)
 	q := url.Values{}
 	q.Add("lang", lang.String())
@@ -65,14 +86,14 @@ func dataEndpointQuery(dataType string, deviceID int, lang Language, services ..
 	return e, q
 }
 
-func (hc *HTTPClient) RealData(deviceID int, lang Language, services ...string) ([]redgiant.RealMeasurement, error) {
+func (hc *Client) RealData(deviceID int, lang redgiant.Language, services ...string) ([]redgiant.RealMeasurement, error) {
 	endpoint, q := dataEndpointQuery("real", deviceID, lang, services)
 	var rms []redgiant.RealMeasurement
-	return rms, hc.getAPI(, q, &rms)
+	return rms, hc.getAPI(endpoint, q, &rms)
 }
 
-func (hc *HTTPClient) DirectData(deviceID int, lang Language, services ...string) ([]redgiant.DirectMeasurement, error) {
+func (hc *Client) DirectData(deviceID int, lang redgiant.Language, services ...string) ([]redgiant.DirectMeasurement, error) {
 	endpoint, q := dataEndpointQuery("direct", deviceID, lang, services)
 	var dms []redgiant.DirectMeasurement
-	return dms, hc.getAPI(, q, &dms)
+	return dms, hc.getAPI(endpoint, q, &dms)
 }
