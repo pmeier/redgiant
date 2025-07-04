e689cccf9956ad019a5f21dbb9efc5efc8c4b5db
Philip Meier <github.pmeier@posteo.de>
2025-07-04 11:08:36 +0200

    switch to blessed mapstructure fork

--- internal/config/config.go
+++ internal/config/config.go
@@ -13,7 +13,7 @@ import (
 
 	"github.com/Masterminds/sprig/v3"
 	"github.com/go-playground/validator/v10"
-	"github.com/mitchellh/mapstructure"
+	"github.com/go-viper/mapstructure/v2"
 	"github.com/rs/zerolog"
 	"github.com/spf13/viper"
 	"golang.org/x/term"
